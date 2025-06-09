483e014b4889c67cfdea61d48535af4623c84987
Christian Jay Reyes <christianjayreyes6@gmail.com>
2025-06-09 22:37:21 +0800

    Hotel Owner

---
+++ client/src/pages/hotelOwner/Layout.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import NavBar from '../../components/hotelOwner/NavBar'
+
+const Layout = () => {
+  return (
+    <div className='flex flex-col h-screen'>
+        <NavBar />
+
+    </div>
+  )
+}
+
+export default Layout

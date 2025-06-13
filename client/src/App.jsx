89726db9d0b3cd3644642b5e511b1e4c9476af63
Christian Jay Reyes <christianjayreyes6@gmail.com>
2025-06-13 14:11:48 +0800

    Owner Dashboard

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,6 +6,11 @@ import Footer from './components/Footer'
 import AllRooms from './pages/AllRooms'
 import RoomDetails from './pages/RoomDetails'
 import MyBookings from './pages/MyBookings'
+import Layout from './pages/hotelOwner/Layout'
+import Dashboard from './pages/hotelOwner/Dashboard'
+import AddRoom from './pages/hotelOwner/AddRoom'
+import ListRoom from './pages/hotelOwner/ListRoom'
+
 
 
 const App = () => {
@@ -20,9 +25,11 @@ const App = () => {
         <Route path='/rooms' element={<AllRooms/>} />
         <Route path='/rooms/:id' element={<RoomDetails/>} />
         <Route path='/my-bookings' element={<MyBookings/>} />
-        <Route path='/owner' element={<Layout/>}/>
-
-
+        <Route path='/owner' element={<Layout/>}>
+            <Route index element={<Dashboard />} />
+            <Route path="add-room" element={<AddRoom />} />
+            <Route path="list-room" element={<ListRoom />} />
+        </Route>
       </Routes>
     </div>
     <Footer />

d3040f2f2f0bc3132f6d90430a938a0723d74a0a
Christian Jay Reyes <christianjayreyes6@gmail.com>
2025-11-19 22:52:23 +0800

    Fixed Google Authentication

--- server/configs/passport.js
+++ server/configs/passport.js
@@ -9,7 +9,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "https://rrh-backend.vercel.app/api/auth/google/callback",
+      callbackURL: "https://rosario-resort-and-hotel.vercel.app/api/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
